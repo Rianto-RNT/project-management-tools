fc0dcb824ed15ec031c1d8a7c7e1da9f4adf494b
Rianto <rianto.rnt@gmail.com>
2021-11-27 18:06:06 +0800

    Turn a Class into Entity

---
+++ project-management/src/test/java/com/rian/pma/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rian.pma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
